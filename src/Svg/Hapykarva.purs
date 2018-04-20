18ba0e3fcdc0c2a828e4ae537ff9ae4ac616ed49
Tero Laxström <tlax@neonpeons.com>
2018-04-20 23:32:23 +0300

    Path building implemented

--- src/Svg/Hapykarva.purs
+++ src/Svg/Hapykarva.purs
@@ -4,8 +4,10 @@ import Prelude
 import Halogen as H
 import Halogen.HTML as HH
 import Data.Maybe (Maybe(..))
-import Data.Array (length, foldr, head)
+import Data.Array (head, tail, foldr, length, range, zip, cons)
 import Data.Int (toNumber)
+import Data.Tuple (Tuple(..), fst, snd)
+import Data.String (joinWith)
 
 type State = 
     { data :: Array Number
@@ -35,27 +37,42 @@ hapykarva =
         let
             width = 100
             height = 100
+
             elem = HH.elementNS (H.Namespace "http://www.w3.org/2000/svg") <<< H.ElemName
             attr = HH.attr <<< H.AttrName
+
             spc = width / (length state.data - 1)
             yMin = foldr min 0.0 state.data
             yMax = foldr max 0.0 state.data
             k = (negate <<< toNumber) height / (yMax - yMin)
             c = negate (k * yMax)
             interpolate x = k * x + c
+
+            xRange = map (\x -> spc * x) $ range 0 (length state.data - 1)
+            points = zip xRange state.data
+            path = moveTo (head points) <> drawLines (tail points)
+                where
+                showPoint :: Tuple Int Number -> String
+                showPoint p = show (fst p) <> "," <> show (interpolate (snd p))
+
+                moveTo :: Maybe (Tuple Int Number) -> String
+                moveTo (Nothing) = ""
+                moveTo (Just p) = "M" <> showPoint p
+
+                drawLines :: Maybe (Array (Tuple Int Number)) -> String
+                drawLines Nothing = ""
+                drawLines (Just ps) = joinWith "" $ map (\p -> "L" <> showPoint p) ps
         in
             elem "svg"
                 [ attr "viewBox" $ "0 0 " <> show width <> " " <> show height
                 , attr "class" "Icon Icon-foo"
                 ]
                 [ elem "path"
-                    [ attr "d" $
-                        "M" <> show (0 * spc) <> "," <> show (interpolate 10.0) <>
-                        "L" <> show (1 * spc) <> "," <> show (interpolate 22.0) <>
-                        "L" <> show (2 * spc) <> "," <> show (interpolate (-11.0)) <>
-                        "L" <> show (3 * spc) <> "," <> show (interpolate 30.0)
+                    [ attr "d" $ path
                     , attr "fill" "none"
                     , attr "stroke" "grey"
+                    , attr "stroke-linecap" "round"
+                    , attr "stroke-linejoin" "round"
                     ]
                     []
                 ]
