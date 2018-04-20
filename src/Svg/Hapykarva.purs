ebc261506eba0974b63cc8b29c0f4fb5d441ef82
Tero Laxström <tlax@neonpeons.com>
2018-04-20 22:41:19 +0300

    Working renderer
    Data is hard coded and SVG Path is not yet created

--- src/Svg/Hapykarva.purs
+++ src/Svg/Hapykarva.purs
@@ -4,7 +4,8 @@ import Prelude
 import Halogen as H
 import Halogen.HTML as HH
 import Data.Maybe (Maybe(..))
-import Data.Array (length)
+import Data.Array (length, foldr, head)
+import Data.Int (toNumber)
 
 type State = 
     { data :: Array Number
@@ -27,23 +28,35 @@ hapykarva =
     where
  
     initialState :: State
-    initialState = { data: [0.0, 2.0, -1.0, 0.0] }
+    initialState = { data: [10.0, 22.0, -11.0, 30.0] }
 
     render :: State -> H.ComponentHTML Query
     render state =
         let
             width = 100
             height = 100
-            svgElem = HH.elementNS $ H.Namespace "http://www.w3.org/2000/svg"
-            svag name = svgElem (H.ElemName name)
-            xSpacing = width / (length state.data - 1)
+            elem = HH.elementNS (H.Namespace "http://www.w3.org/2000/svg") <<< H.ElemName
+            attr = HH.attr <<< H.AttrName
+            spc = width / (length state.data - 1)
+            yMin = foldr min 0.0 state.data
+            yMax = foldr max 0.0 state.data
+            k = (negate <<< toNumber) height / (yMax - yMin)
+            c = negate (k * yMax)
+            interpolate x = k * x + c
         in
-            svag "svg"
-                [ HH.attr (H.AttrName "viewBox") ("0 0 " <> show width <> " " <> show height)
-                , HH.attr (H.AttrName "class") "Icon Icon-foo"
+            elem "svg"
+                [ attr "viewBox" $ "0 0 " <> show width <> " " <> show height
+                , attr "class" "Icon Icon-foo"
                 ]
-                [ svag "path"
-                    [HH.attr (H.AttrName "d") "M10 10 H 90 V 90 H 10 L 10 10" ]
+                [ elem "path"
+                    [ attr "d" $
+                        "M" <> show (0 * spc) <> "," <> show (interpolate 10.0) <>
+                        "L" <> show (1 * spc) <> "," <> show (interpolate 22.0) <>
+                        "L" <> show (2 * spc) <> "," <> show (interpolate (-11.0)) <>
+                        "L" <> show (3 * spc) <> "," <> show (interpolate 30.0)
+                    , attr "fill" "none"
+                    , attr "stroke" "grey"
+                    ]
                     []
                 ]
     
